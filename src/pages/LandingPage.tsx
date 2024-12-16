ad13ad288e1524037ede76b62e2d7d69b897f647
desmondyst <desmondyong06@gmail.com>
2024-12-16 20:56:43 +0800

    💄 add margin top to viewpdfpage and landingpage as navbar is fixed

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,106 +1,154 @@
 import { Box, Divider, Skeleton, Stack, Typography } from "@mui/material";
-import { theme } from "../theme"
+import { theme } from "../theme";
 import { ReactElement } from "react";
 
 const LandingPage = () => {
-  return (
-    <Box sx={{ flex: 1, flexDirection: "column", alignItems: "center", background: theme.colors.secondary }}>
-      <Box sx={{ background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', flex: 1, padding: 10, marginBottom: 5 }}>
-        <Typography
-          fontSize={"4rem"}
-          fontFamily={"Poppins, sans-serif"}
-          textAlign={"center"}
-          fontWeight={"bold"}
-          sx={{ color: theme.colors.secondary }}
+    return (
+        <Box
+            sx={{
+                flex: 1,
+                flexDirection: "column",
+                alignItems: "center",
+                background: theme.colors.secondary,
+                mt: "5rem",
+            }}
         >
-          EduTrack
-        </Typography>
-        <Typography
-          fontSize={"2.8rem"}
-          fontFamily={"Poppins, sans-serif"}
-          textAlign={"center"}
-          sx={{ color: theme.colors.secondary }}
-        >
-          measure the impact of education practices on learning outcomes.
-        </Typography>
-      </Box>
-      <Typography
-        fontSize={"2.8rem"}
-        fontWeight={"bold"}
-        fontFamily={"monospace"}
-        textAlign={"center"}
-        sx={{ textDecoration: "underline" }}
-        
-      >
-        Features
-      </Typography>
+            <Box
+                sx={{
+                    background:
+                        "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+                    flex: 1,
+                    padding: 10,
+                    marginBottom: 5,
+                }}
+            >
+                <Typography
+                    fontSize={"4rem"}
+                    fontFamily={"Poppins, sans-serif"}
+                    textAlign={"center"}
+                    fontWeight={"bold"}
+                    sx={{ color: theme.colors.secondary }}
+                >
+                    EduTrack
+                </Typography>
+                <Typography
+                    fontSize={"2.8rem"}
+                    fontFamily={"Poppins, sans-serif"}
+                    textAlign={"center"}
+                    sx={{ color: theme.colors.secondary }}
+                >
+                    measure the impact of education practices on learning
+                    outcomes.
+                </Typography>
+            </Box>
+            <Typography
+                fontSize={"2.8rem"}
+                fontWeight={"bold"}
+                fontFamily={"monospace"}
+                textAlign={"center"}
+                sx={{ textDecoration: "underline" }}
+            >
+                Features
+            </Typography>
 
+            <Stack divider={<Divider flexItem />}>
+                <FeatureStack
+                    image={
+                        <Skeleton
+                            variant="rectangular"
+                            width={400}
+                            height={300}
+                            sx={{ marginLeft: "auto", marginRight: "auto" }}
+                        />
+                    }
+                    title={"Question Bank"}
+                    desc={
+                        "Auto-generate corpus of questions from past year papers."
+                    }
+                />
 
-      <Stack divider={<Divider flexItem />}>
+                <FeatureStack
+                    image={
+                        <Skeleton
+                            variant="rectangular"
+                            width={400}
+                            height={300}
+                            sx={{ marginLeft: "auto", marginRight: "auto" }}
+                        />
+                    }
+                    title={"Knowledge Graph"}
+                    desc={
+                        "This is a map of all knowledge used to infer the coverage and scope of different educational activities."
+                    }
+                />
 
-        <FeatureStack
-          image={<Skeleton variant="rectangular" width={400} height={300} sx={{ marginLeft: "auto", marginRight: "auto" }} />}
-          title={"Question Bank"}
-          desc={"Auto-generate corpus of questions from past year papers."}
-        />
+                <FeatureStack
+                    image={
+                        <Skeleton
+                            variant="rectangular"
+                            width={400}
+                            height={300}
+                            sx={{ marginLeft: "auto", marginRight: "auto" }}
+                        />
+                    }
+                    title={"Assessment Generator"}
+                    desc={
+                        "The knowledge graph and the question bank would be leveraged to generate assessments."
+                    }
+                />
 
-        <FeatureStack
-          image={<Skeleton variant="rectangular" width={400} height={300} sx={{ marginLeft: "auto", marginRight: "auto" }} />}
-          title={"Knowledge Graph"}
-          desc={"This is a map of all knowledge used to infer the coverage and scope of different educational activities."}
-        />
+                <FeatureStack
+                    image={
+                        <Skeleton
+                            variant="rectangular"
+                            width={400}
+                            height={300}
+                            sx={{ marginLeft: "auto", marginRight: "auto" }}
+                        />
+                    }
+                    title={"Assessment Metric"}
+                    desc={
+                        "Each student's performance would be normalized by generating a metric that is independent of the assessment."
+                    }
+                />
 
-        <FeatureStack
-          image={<Skeleton variant="rectangular" width={400} height={300} sx={{ marginLeft: "auto", marginRight: "auto" }} />}
-          title={"Assessment Generator"}
-          desc={"The knowledge graph and the question bank would be leveraged to generate assessments."}
-        />
-
-        <FeatureStack
-          image={<Skeleton variant="rectangular" width={400} height={300} sx={{ marginLeft: "auto", marginRight: "auto" }} />}
-          title={"Assessment Metric"}
-          desc={"Each student's performance would be normalized by generating a metric that is independent of the assessment."}
-        />
-
-        <FeatureStack
-          image={<Skeleton variant="rectangular" width={400} height={300} sx={{ marginLeft: "auto", marginRight: "auto" }} />}
-          title={"Intuitive UI"}
-          desc={"Use intuitive UI/UX to navigate the site."}
-        />
-
-      </Stack>
-    </Box>
-  );
+                <FeatureStack
+                    image={
+                        <Skeleton
+                            variant="rectangular"
+                            width={400}
+                            height={300}
+                            sx={{ marginLeft: "auto", marginRight: "auto" }}
+                        />
+                    }
+                    title={"Intuitive UI"}
+                    desc={"Use intuitive UI/UX to navigate the site."}
+                />
+            </Stack>
+        </Box>
+    );
 };
 
 interface FeatureStackProps {
-  image: ReactElement,
-  title: String,
-  desc: String,
+    image: ReactElement;
+    title: String;
+    desc: String;
 }
 
 const FeatureStack: React.FC<FeatureStackProps> = ({ image, title, desc }) => {
-  return (
-    <Stack direction={{ sm: 'column', md: 'row' }} spacing={2} margin={5}>
-      <Box flex={1} >
-        {image}
-      </Box>
-      <Stack sx={{ flex: 2 }} >
-        <Typography
-          fontSize={"2.8rem"}
-          textAlign={"center"}
-        >
-          {title}
-        </Typography>
-        <Typography
-          fontSize={"1.3rem"}
-          textAlign={"center"}
-        >
-          {desc}
-        </Typography>
-      </Stack>
-    </Stack>
-  );
-}
+    return (
+        <Stack direction={{ sm: "column", md: "row" }} spacing={2} margin={5}>
+            <Box flex={1}>{image}</Box>
+            <Stack sx={{ flex: 2 }}>
+                <Typography fontSize={"2.8rem"} textAlign={"center"}>
+                    {title}
+                </Typography>
+                <Typography fontSize={"1.3rem"} textAlign={"center"}>
+                    {desc}
+                </Typography>
+            </Stack>
+        </Stack>
+    );
+};
 
 export default LandingPage;
--- src/pages/ViewPdfPage.tsx
+++ src/pages/ViewPdfPage.tsx
@@ -1,4 +1,11 @@
-import { Box, CircularProgress, Typography, Stack, Button, Divider } from "@mui/material";
+import {
+    Box,
+    CircularProgress,
+    Typography,
+    Stack,
+    Button,
+    Divider,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Api, { ApiError } from "../api/Api";
@@ -9,44 +16,52 @@ import "../App.css";
 const ViewPdfPage = () => {
     const location = useLocation(); // Used for auto redirection to the paper
 
-    const [isFetchingTitleList, setIsFetchingTitleList] = useState<Boolean>(true);
+    const [isFetchingTitleList, setIsFetchingTitleList] =
+        useState<Boolean>(true);
     const [titleList, setTitleList] = useState<String[]>([]);
     const [selectedPaper, setSelectedPaper] = useState<number>(-1); // TODO: Check with backend to see how we want to specify the paper
 
     useEffect(() => {
-        Api.getPdfTitleList().then((response) => {
-            setIsFetchingTitleList(false);
-            const titleList = response.data; // Due to state updating rules, we temporarily overwrite the titleList variable in this scope to make sure that the program runs as intended.
-            setTitleList(titleList);
+        Api.getPdfTitleList()
+            .then((response) => {
+                setIsFetchingTitleList(false);
+                const titleList = response.data; // Due to state updating rules, we temporarily overwrite the titleList variable in this scope to make sure that the program runs as intended.
+                setTitleList(titleList);
 
-            // Auto redirection
-            const title = location?.state?.response?.data?.title ?? "";
-            if (title) { // If there is a paper that requires to be automatically redirected
-                setSelectedPaper(response.data.indexOf(title));
-            }
-        }).catch((error) => {
-            if (error instanceof ApiError) {
-                // Handle specific API errors
-                console.error("API Error uploading file:", error.message);
-            } else {
-                // Handle any unexpected errors
-                console.error("Unexpected error uploading file:", error);
-            }
-            setIsFetchingTitleList(false);
-        })
+                // Auto redirection
+                const title = location?.state?.response?.data?.title ?? "";
+                if (title) {
+                    // If there is a paper that requires to be automatically redirected
+                    setSelectedPaper(response.data.indexOf(title));
+                }
+            })
+            .catch((error) => {
+                if (error instanceof ApiError) {
+                    // Handle specific API errors
+                    console.error("API Error uploading file:", error.message);
+                } else {
+                    // Handle any unexpected errors
+                    console.error("Unexpected error uploading file:", error);
+                }
+                setIsFetchingTitleList(false);
+            });
     }, []);
 
     const SelectPaperPanel = () => {
         return (
-            <Stack flex={1} divider={<Divider flexItem />} >
-                {titleList.map((title, index) => 
-                    <Button 
-                        sx={{ justifyContent: "flex-start", height: "5rem", flexShrink: 0 }}
+            <Stack flex={1} divider={<Divider flexItem />}>
+                {titleList.map((title, index) => (
+                    <Button
+                        sx={{
+                            justifyContent: "flex-start",
+                            height: "5rem",
+                            flexShrink: 0,
+                        }}
                         onClick={() => setSelectedPaper(index)}
                     >
                         {title}
                     </Button>
-                )}
+                ))}
             </Stack>
         );
     };
@@ -55,9 +70,10 @@ const ViewPdfPage = () => {
         <Box
             sx={{
                 flex: 1,
-                height: 'calc(100vh - 5rem)', // Remaining height below the NavBar
-                display: 'flex',
+                height: "calc(100vh - 5rem)", // Remaining height below the NavBar
+                display: "flex",
                 backgroundColor: theme.colors.secondary, // Example background color
+                mt: "5rem", // Margin top to prevent overlap with NavBar
             }}
         >
             {/* Left portion for selection */}
@@ -67,20 +83,35 @@ const ViewPdfPage = () => {
                     flex: 1,
                     display: "flex",
                     background: theme.colors.secondary,
-                    overflowY: 'auto', // Enable vertical scrolling
+                    overflowY: "auto", // Enable vertical scrolling
                 }}
             >
                 {isFetchingTitleList ? (
                     <Box sx={{ flex: 1, alignSelf: "center" }}>
-                        <CircularProgress size={"100px"} sx={{ color: theme.colors.highlight1, alignSelf: "center" }} />
+                        <CircularProgress
+                            size={"100px"}
+                            sx={{
+                                color: theme.colors.highlight1,
+                                alignSelf: "center",
+                            }}
+                        />
                     </Box>
                 ) : (
                     <SelectPaperPanel />
                 )}
             </Box>
-            
+
             {/* Vertical Divider */}
-            <Divider orientation="vertical" flexItem sx={{ backgroundColor: theme.colors.highlight1, width: "0.25rem", borderRadius: 3, margin: 1 }} />
+            <Divider
+                orientation="vertical"
+                flexItem
+                sx={{
+                    backgroundColor: theme.colors.highlight1,
+                    width: "0.25rem",
+                    borderRadius: 3,
+                    margin: 1,
+                }}
+            />
 
             {/* Right portion for content */}
             <Box
@@ -94,13 +125,14 @@ const ViewPdfPage = () => {
                 {selectedPaper !== -1 ? (
                     <ContentTableFromTitle title={titleList[selectedPaper]} />
                 ) : (
-                    <Box 
-                        sx={{ 
+                    <Box
+                        sx={{
                             flex: 1,
-                            display: 'flex',
+                            display: "flex",
                             justifyContent: "center",
-                            alignItems: 'center',
-                        }}>
+                            alignItems: "center",
+                        }}
+                    >
                         <Typography
                             fontSize={"2rem"}
                             ml={2}
